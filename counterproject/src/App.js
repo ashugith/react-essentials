45efeabb56645d33ab4bf8ab231b70898d2562e6
Ashu Singh <singh.ashu04072001@gmail.com>
2024-01-24 20:00:48 +0530

    commit to change in counter app
    committing to change in counter app

--- counterproject/src/App.js
+++ counterproject/src/App.js
@@ -1,42 +1,39 @@
 import './App.css';
-import { useState } from 'react';
-
-
+ import { useState } from 'react';
 function App() {
   const[count , setCount] = useState(0);
   function decreaseHandler(){
     setCount(count-1);
 
-  }
+   }
   function increaseHandler(){
     setCount(count+1);
 
-  }
-  function resetHandler(){
+  }  function resetHandler(){
     setCount(0);
+  } 
+   return (
+   <div className="App">
+     <div className='in_de'>Increment && Decrement</div>
+     <div className='btn'>
+       <button onClick={increaseHandler}>
+         +
+       </button>
+       <div className='counter'>
+         {count}
 
+         </div>
+       <button onClick={decreaseHandler}>
+       -
+     </button>
+     </div>
+     <button onClick={resetHandler} className='reset'>
+       reset
+       </button>
+
+</div>
+   )
+  
   }
-  return (
-    <div className="App">
-      <div className='in_de'>Increment && Decrement</div>
-      <div className='btn'>
-        <button onClick={increaseHandler}>
-          +
-        </button>
-        <div className='counter'>
-          {count}
-
-        </div>
-        <button onClick={decreaseHandler}>
-        -
-      </button>
-      </div>
-      <button onClick={resetHandler} className='reset'>
-        reset
-        </button>
-
-    </div>
-  );
-}
 
 export default App;
