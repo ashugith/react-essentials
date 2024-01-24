e53e0c74f6ec29f35dbd555b0a6c0f5c764d1a1a
Ashu Singh <singh.ashu04072001@gmail.com>
2024-01-24 21:40:47 +0530

    commit to project1
    committing to project1

---
+++ React_project1/temperatureapp/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+// import { useState } from 'react';
+import Temp from './components/Temp';
+function App () {
+  return(
+    <div>
+      <Temp></Temp>
+    </div>
+  )
+}
+export default App;
---
+++ React_project1/temperatureapp/src/components/Temp.js
@@ -0,0 +1,32 @@
+import './Temp.css';
+import { useState } from "react"
+
+const Temp = () => {
+    const [temp , setTemp] = useState(0)
+    const [color, setColor] = useState("cold")
+    function incrementTemp () {
+        setTemp(temp + 1)
+        if(temp>= 20){
+            setColor("hot")
+        }
+    }
+    function decrementTemp () {
+        setTemp(temp - 1)
+        if(temp <= 20)
+        setColor("cold")
+    }
+    return(
+        <div className='app-container'>
+            <div className='temp-display-container'>
+                <div className={`temp-display ${color}`}>
+                    {temp} °C
+                </div>
+            </div>
+            <div className='button-container'>
+                <button onClick={incrementTemp}>+</button>
+                <button onClick={decrementTemp}>-</button>
+            </div>
+        </div>
+    )
+}
+export default Temp;
