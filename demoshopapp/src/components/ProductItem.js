baa9ebda7f150fe6bd069a30692f9d25eeb2ae2d
Ashu Singh <singh.ashu04072001@gmail.com>
2024-01-27 23:12:48 +0530

    commit to demoshopapp
    committing to demoshopapp

---
+++ demoshopapp/src/components/ProductItem.js
@@ -0,0 +1,16 @@
+import ProductDate from './ProductDate';
+import Card from './Card';
+import './ProductItem.css';
+
+const ProductItem = (props) => {
+  return (
+    <Card className='product-item'>
+      <ProductDate date={props.date} />
+      <div className='product-item__description'>
+        <h2> {props.title} </h2>
+      </div>
+    </Card>
+  );
+}
+
+export default ProductItem;
