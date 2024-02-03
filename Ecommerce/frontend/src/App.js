068059ea5423dcad0e4b31b44fdf2984b2651ffa
Ashu Singh <singh.ashu04072001@gmail.com>
2024-02-03 23:08:04 +0530

    Commit to Shopper project
    Committing to Shopper project

---
+++ Ecommerce/frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './Navbar/Navbar'
+
+function App() {
+  return (
+    <div>
+      <Navbar>
+        
+      </Navbar>
+      
+
+      
+    </div>
+  );
+  
+}
+
+export default App;
