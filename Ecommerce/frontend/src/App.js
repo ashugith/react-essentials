61a54bf820ad153351881fd6afa798e6fae124e1
Ashu Singh <singh.ashu04072001@gmail.com>
2024-02-04 18:43:01 +0530

    Commit to change in app.js & navbar.jsx
    Committing to change in app.js & navbar.jsx.

--- Ecommerce/frontend/src/App.js
+++ Ecommerce/frontend/src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Navbar from './Navbar/Navbar'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Navbar>
-        
-      </Navbar>
+     <BrowserRouter>
+     <Navbar/>
+     <Routes>
+      <Route path='/' element={<Shop/>}/>
+      <Route path='/mens' element={<ShopCategory category="mens"/>}/>
+      <Route path='/womens' element={<ShopCategory category="womens"/>}/>
+      <Route path='/kids' element={<ShopCategory category="kids"/>}/>
+      <Route path="/product" element={<Product/>}>
+        < Route path=':productId' element={<Product/>}/>
+      </Route>
+      <Route path='/cart' element={<cart/>}/>
+      <Route path='/login' element={<LoginSignup/>}/>
+     </Routes>
+     </BrowserRouter>
       
 
       
