82595b8212305569d50aeba7f23865ca71888420
Ashu Singh <singh.ashu04072001@gmail.com>
2024-02-08 22:48:31 +0530

    Commit to change in Shop.jsx & popular.jsx
    Committing to change in shop.jsx & popular.jsx

--- Ecommerce/frontend/src/Pages/Shop.jsx
+++ Ecommerce/frontend/src/Pages/Shop.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Popular from '../Popular/Popular'
+import Hero from '../Hero/Hero'
 
 const Shop  = () => {
   return (
     <div>
+      <Hero/>
+      <Popular/>
       
     </div>
   )
