018803cb08870cdebbad6adf8ae12619e07a8ce1
Ashu Singh <singh.ashu04072001@gmail.com>
2024-02-17 22:55:16 +0530

    Commit to update in shop category
    Committing to update in shop category.

--- Ecommerce/frontend/src/Pages/ShopCategory.jsx
+++ Ecommerce/frontend/src/Pages/ShopCategory.jsx
@@ -1,9 +1,9 @@
 import React,{useContext} from 'react'
 import './CSS/ShopCategory.css'
-import all_product from '../Assets/all_product'
 import { ShopContext } from '../Context/ShopContext'
 import dropdown_icon from '../Assets/dropdown_icon.png'
 import Item from '../Item/Item'
+
 
 
 const ShopCategory = (props) => {
@@ -20,16 +20,18 @@ const ShopCategory = (props) => {
         </div>
       </div>
       <div className="shopcategory-products">
-        {all_product.map((item,i)=>{
-          if (props.category===item.category) {
-            return <Item key={i}  id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
-
+        {all_product.map((item,i)=> {
+          if(props.category===item.category){
+            return <Item key={i}  id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
           }
           else{
             return null;
           }
-          
-        })}
+        }
+        
+        
+        
+        )}
       </div>
       
     </div>
