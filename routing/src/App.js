2adf39490dd90ee07a93602e1ba84831b89656a9
Ashu Singh <singh.ashu04072001@gmail.com>
2024-02-18 22:59:09 +0530

    Commit to Routing concept
    Committing to Routing concept.

--- routing/src/App.js
+++ routing/src/App.js
@@ -1,9 +1,22 @@
-import {Route, Routes} from 'react-router-dom'
+import {Route, Routes , BrowserRouter} from 'react-router-dom'
 import './App.css';
+import Navbar from './components/Layout/Navbar';
+import Home from './components/Pages/Home';
+import About from './components/Pages/About';
+
+
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/home' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        {/* <Route path='/contact' element={<Contact/>}></Route> */}
+      </Routes>
+      </BrowserRouter>
       
     </div>
   );
--- routing/src/components/Layout/Navbar.js
+++ routing/src/components/Layout/Navbar.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
     <div className='header'>
         <div className='navbar'>
-            <div>Home</div>
-            <div>About</div>
-            <div>Contact</div>
+            <NavLink to='/home'><div className='nav'>
+                Home
+                </div></NavLink>
+            <NavLink to='/about'><div className='nav'>
+                About
+                </div></NavLink>
+           {/* <NavLink to='/contact'><div className='nav'>
+            Contact
+            </div></NavLink> */}
         </div>
       
     </div>
