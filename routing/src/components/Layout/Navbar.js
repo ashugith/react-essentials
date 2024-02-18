bc7bfe086c7885cce20bc601e78c0c8ad18054d9
Ashu Singh <singh.ashu04072001@gmail.com>
2024-02-18 22:01:47 +0530

    Commit to change in routing
    Committing to change in routing

---
+++ routing/src/components/Layout/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='header'>
+        <div className='navbar'>
+            <div>Home</div>
+            <div>About</div>
+            <div>Contact</div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
