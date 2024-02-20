46497d95dbb2b3fb95e4d540aed1e8cea322a657
Ashu Singh <singh.ashu04072001@gmail.com>
2024-02-20 21:57:38 +0530

    Commit to change in Data,js
    Committing to change in Data.js

--- plan-with-love-starter-app/src/App.js
+++ plan-with-love-starter-app/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
+
+import Data from './Data'
+import React, { useState } from 'react';
+import Tours from './components/Tours'
 
 const App = () => {
+  const [tours , setTours] = useState(Data)
   return (
     <div>
       <h2>Plan with love</h2>
-      <Tours></Tours>
+      <Tours tours={tours}></Tours>
       
     </div>
   )
---
+++ plan-with-love-starter-app/src/Data.js
@@ -0,0 +1,16 @@
+const Data = [
+    {
+        id: 1 ,
+        name: "Agra",
+        info: "Agra is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres (140 mi) south-east of the national capital Delhi .",
+        image: "https://unsplash.com/photos/taj-mahal-india-iWMfiInivp4",
+        price: "25,000"
+    },
+    {
+        id: 2 ,
+        name: "Jaipur",
+        info: "Agra is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres (140 mi) south-east of the national capital Delhi .",
+        image: "https://unsplash.com/photos/taj-mahal-india-iWMfiInivp4",
+        price: "30,000"
+    }
+]
