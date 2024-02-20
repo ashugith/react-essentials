2510893245b5367fd43895c8fb15403bbc8683fb
Ashu Singh <singh.ashu04072001@gmail.com>
2024-02-20 23:46:53 +0530

    Commit to Update in Tours.js
    Committing to Update in Tours.js

--- plan-with-love-starter-app/src/components/Tours.js
+++ plan-with-love-starter-app/src/components/Tours.js
@@ -1,11 +1,23 @@
-function Tours () {
+function Tours ({tours}) {
     return (
         <div>
             <div>
                 <h2>Plan with Love</h2>
             </div>
+            <div>
+                {
+                    tours.map((tours)=> {
+                        return <Card>
+
+                        </Card>
+
+                    })
+                }
+            </div>
         </div>
-    )
+        
+        
+    );
 }
-export default Tours
+export default Tours;
   
