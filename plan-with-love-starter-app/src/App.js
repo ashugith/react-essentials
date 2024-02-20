730bb2c1248861b127eeecb6151c598ab2e20e7a
Ashu Singh <singh.ashu04072001@gmail.com>
2024-02-20 23:29:30 +0530

    Commit to change in Tours.js
    Committing to change in Tours.js

--- plan-with-love-starter-app/src/App.js
+++ plan-with-love-starter-app/src/App.js
@@ -7,7 +7,6 @@ const App = () => {
   const [tours , setTours] = useState(Data)
   return (
     <div>
-      <h2>Plan with love</h2>
       <Tours tours={tours}></Tours>
       
     </div>
--- plan-with-love-starter-app/src/components/Tours.js
+++ plan-with-love-starter-app/src/components/Tours.js
@@ -1,4 +1,11 @@
-
-function Tours (){
-    
+function Tours () {
+    return (
+        <div>
+            <div>
+                <h2>Plan with Love</h2>
+            </div>
+        </div>
+    )
 }
+export default Tours
+  
