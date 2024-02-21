5ff63e617ed08c6f27c68fe6c67bb9002296b369
Ashu Singh <singh.ashu04072001@gmail.com>
2024-02-21 22:37:34 +0530

    Commit to Update in Gallery.js
    Committing to Update in Gallery.js

--- project2app/src/component/Pages/Gallery.js
+++ project2app/src/component/Pages/Gallery.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Car4 from '../../assets/Car4.jpeg'
+import Car5 from '../../assets/Car5.jpeg'
+import Car6 from '../../assets/Car6.jpeg'
+import Car7 from '../../assets/Car7.jpeg'
 
 const Gallery = () => {
   return (
     <div className='row'>
       <div className='col-3'>
       <div className="card" style={{width: '18rem'}}>
-  <img src="..." className="card-img-top" alt="..."/>
+  <img src={Car4} className="card-img-top" height='180px' alt="car4"/>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -17,7 +21,7 @@ const Gallery = () => {
 </div>
 <div className='col-3'>
 <div className="card" style={{width: ' 18rem'}}>
-  <img src="..." className="card-img-top" alt="..."/>
+  <img src={Car5} className="card-img-top" height='180px' alt="car5"/>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -28,7 +32,7 @@ const Gallery = () => {
 </div>
 <div className='col-3'>
 <div className="card" style={{width: ' 18rem'}}>
-  <img src="..." className="card-img-top" alt="..."/>
+  <img src={Car6} className="card-img-top" height='180px' alt="car6"/>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -39,7 +43,7 @@ const Gallery = () => {
 </div>
 <div className='col-3'>
 <div className="card" style={{width: ' 18rem'}}>
-  <img src="..." className="card-img-top" alt="..."/>
+  <img src={Car7} className="card-img-top" height='180px' alt="car7"/>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -49,6 +53,54 @@ const Gallery = () => {
         
       
     </div>
+    <div className='col-3 mt-5'>
+      <div className="card" style={{width: '18rem'}}>
+  <img src={Car4} className="card-img-top" height='180px' alt="car4"/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+
+      </div>
+    
+</div>
+
+<div className='col-3 mt-5' >
+<div className="card" style={{width: ' 18rem'}}>
+  <img src={Car5} className="card-img-top" height='180px' alt="car5"/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+</div>
+<div className='col-3 mt-5'>
+<div className="card" style={{width: ' 18rem'}}>
+  <img src={Car6} className="card-img-top" height='180px' alt="car6"/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+</div>
+<div className='col-3 mt-5'>
+<div className="card" style={{width: ' 18rem'}}>
+  <img src={Car7} className="card-img-top" height='180px' alt="car7"/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+        
+      
+    </div>
+
 
 </div>
 
