e0fe66ab813e0dbeb5f6925df8d971d5c892562c
Ashu Singh <singh.ashu04072001@gmail.com>
2024-02-21 20:08:55 +0530

    Commit to change in Project2
    Committing to change in Project2

--- project2app/src/component/Pages/Home.js
+++ project2app/src/component/Pages/Home.js
@@ -1,19 +1,20 @@
 import React from 'react'
-import octavia from '../../assets/octavia.jpeg'
-
+import Car1 from '../../assets/Car1.jpeg';
+import Car2 from '../../assets/Car2.jpeg'
+import Car3 from '../../assets/Car3.jpeg'
 const Home = () => {
   return (
     <div>
         <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
   <div className="carousel-inner">
-    <div className="carousel-item active" data-bs-interval="10000">
-      <img src={octavia} className="d-block w-100" height="645px"alt="octavia"/>
+    <div className="carousel-item active" data-bs-interval="2000">
+      <img src={Car1} className="d-block w-100" height="645px"alt="Car1"/>
     </div>
     <div className="carousel-item" data-bs-interval="2000">
-      <img src={} className="d-block w-100" alt="..."/>
+      <img src={Car2} className="d-block w-100" alt="Car2"/>
     </div>
     <div className="carousel-item">
-      <img src="..." className="d-block w-100" alt="..."/>
+      <img src={Car3} className="d-block w-100" alt="Car3"/>
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
