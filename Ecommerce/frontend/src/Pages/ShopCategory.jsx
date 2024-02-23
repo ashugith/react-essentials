738ee7a8a1fb170bcbaa2bff935cff3f383ebcab
Ashu Singh <singh.ashu04072001@gmail.com>
2024-02-23 23:27:58 +0530

    Commit to update in Ecommerce
    Committing to update in Ecommerce.

--- Ecommerce/frontend/src/Pages/ShopCategory.jsx
+++ Ecommerce/frontend/src/Pages/ShopCategory.jsx
@@ -20,18 +20,13 @@ const ShopCategory = (props) => {
         </div>
       </div>
       <div className="shopcategory-products">
-        {all_product.map((item,i)=> {
-          if(props.category===item.category){
-            return <Item key={i}  id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-          }
-          else{
-            return null;
-          }
-        }
-        
-        
-        
-        )}
+      {all_product.map((item , i) => {
+  if (props.category === item.category) {
+    return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+  } else {
+    return null;
+  }
+})}
       </div>
       
     </div>
