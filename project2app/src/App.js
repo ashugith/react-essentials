b4c55c878395c4c6141eb1c32eb3c16641d08f21
Ashu Singh <singh.ashu04072001@gmail.com>
2024-02-23 20:56:47 +0530

    Commit to change in Project2
    Committing to change in Project2

--- project2app/src/App.js
+++ project2app/src/App.js
@@ -14,6 +14,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <Routes>
+        <Route path='/' element={<Home/>}></Route>
           <Route path="/home" element={<Home/>}></Route>
           <Route path="/about" element={<About/>}></Route>
           <Route path="/gallery" element={<Gallery/>}></Route>
--- project2app/src/component/Pages/Register.js
+++ project2app/src/component/Pages/Register.js
@@ -1,55 +1,69 @@
-import React from 'react'
+import G from '../../assets/G.jpeg'
+import { useForm } from 'react-hook-form'
+import './Register.css'
+import axios from 'axios'
+
 
 const Register = () => {
-  return (
-    <div>
-        <form class="row g-3">
-  <div class="col-md-6">
-    <label for="inputEmail4" class="form-label">Email</label>
-    <input type="email" class="form-control" id="inputEmail4"/>
-  </div>
-  <div class="col-md-6">
-    <label for="inputPassword4" class="form-label">Password</label>
-    <input type="password" class="form-control" id="inputPassword4"/>
-  </div>
-  <div class="col-12">
-    <label for="inputAddress" class="form-label">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"/>
-  </div>
-  <div class="col-12">
-    <label for="inputAddress2" class="form-label">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
-  </div>
-  <div class="col-md-6">
-    <label for="inputCity" class="form-label">City</label>
-    <input type="text" class="form-control" id="inputCity"/>
-  </div>
-  <div class="col-md-4">
-    <label for="inputState" class="form-label">State</label>
-    <select id="inputState" class="form-select">
-      <option selected>Choose...</option>
-      <option>...</option>
-    </select>
-  </div>
-  <div class="col-md-2">
-    <label for="inputZip" class="form-label">Zip</label>
-    <input type="text" class="form-control" id="inputZip"/>
-  </div>
-  <div class="col-12">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck"/>
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <div class="col-12">
-    <button type="submit" class="btn btn-primary">Sign in</button>
-  </div>
-</form>
-      
-    </div>
-  )
+  const {register, handleSubmit, reset}  = useForm()
+  function saveData (data) {
+    axios.post("http://localhost:8080/customer",data)
+    alert("Your Data Added !!!!")
+    reset ()
 }
 
-export default Register
+
+
+
+  return (
+      <>
+          <img src={G} alt="Gimage" className='img w-100'/>
+          <h1 className="mt-3 mb-5 h1 text-danger">BOOK YOUR CAR</h1>
+          <form onSubmit={handleSubmit(saveData)} className="row g-3 w-50 mx-auto bg-dark rounded-3 p-2 form opacity-75">
+              <div className="col-md-6">
+                  <label htmlFor="name" className="form-label text-warning fs-5">Name</label>
+                  <input type="name" className="form-control" id="name" {...register("name")}/>
+              </div>
+              <div className="col-md-6">
+                  <label htmlFor="email" className="form-label text-warning fs-5">Email</label>
+                  <input type="email" className="form-control" id="email" {...register("email")}/>
+              </div>
+              <div className="col-md-6">
+                  <label htmlFor="pickupaddress" className="form-label text-warning fs-5">Pickup Address</label>
+                  <input type="text" className="form-control" id="pickupaddress" placeholder="1234 Main St" {...register("pickup_address")}/>
+              </div>
+              <div className="col-md-6">
+                  <label htmlFor="destinationaddress" className="form-label text-warning fs-5">Destination Address</label>
+                  <input type="text" className="form-control" id="destinationaddress" placeholder="Apartment, studio, or floor" {...register("destination_address")}/>
+              </div>
+              <div className="col-md-3">
+                  <label htmlFor="contact" className="form-label text-warning fs-5">Contact Number</label>
+                  <input type="tel" className="form-control" id="contact" {...register("contact")}/>
+              </div>
+              <div className="col-md-3">
+                  <label htmlFor="city" className="form-label text-warning fs-5">Destination City</label>
+                  <input type="text" className="form-control" id="city" {...register("city")}/>
+              </div>
+              <div className="col-md-3">
+                  <label htmlFor="state" className="form-label text-warning fs-5">Select State</label>
+                  <select id="state" className="form-select" {...register("state")}>
+                      <option value="">Choose State</option>
+                      <option value="maharashtra">Maharashtra</option>
+                      <option value="madhya pradesh">Madhya Pradesh</option>
+                      <option value="uttar pradesh">Uttar Pradesh</option>
+                      <option value="rajasthan">Rajasthan</option>
+                      <option value="uttarakhand">Uttarakhand</option>
+                  </select>
+              </div>
+              <div className="col-md-3">
+                  <label htmlFor="zipcode" className="form-label text-warning fs-5">Zip</label>
+                  <input type="tel" className="form-control" id="zipcode" {...register("zipcode")}/>
+              </div>
+              <div>
+                  <button type="submit" className="btn btn-warning col-md-12 mb-4 fw-bold fs-4">Book Car</button>
+              </div>
+          </form>
+      </>
+  )
+}
+export default Register;
