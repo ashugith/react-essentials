a2a2f898504caaae05697fc3334fe8dac5336deb
Ashu Singh <singh.ashu04072001@gmail.com>
2024-02-23 23:26:48 +0530

    Commit to update in Project2
    Committing to update in Project2

--- project2app/src/component/Pages/Show.js
+++ project2app/src/component/Pages/Show.js
@@ -1,12 +1,64 @@
-import React from 'react'
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 const Show = () => {
-  return (
-    <div>
-        <h2>I am show</h2>
-      
-    </div>
-  )
-}
 
-export default Show
+
+    const [customer,setCustomer] = useState([])
+
+
+    async function getData () {
+        const response = await axios.get("http://localhost:8080/customer")
+        const result = await response.data
+        setCustomer(result)
+    }
+
+
+    useEffect(()=>{
+        getData()
+    },[])
+    return(
+        <>
+           <h2>Customer Details</h2>
+           <table className="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Customer Name</th>
+                        <th>Customer Email</th>
+                        <th>Pickup Address</th>
+                        <th>Destination Address</th>
+                        <th>Contact Number</th>
+                        <th>City</th>
+                        <th>State</th>
+                        <th>Zipcode</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        customer.map((x)=>{
+                            return(
+                                <tr key={x.id}>
+                                    <td>{x.id}</td>
+                                    <td>{x.name}</td>
+                                    <td>{x.email}</td>
+                                    <td>{x.pickup_address}</td>
+                                    <td>{x.destination_address}</td>
+                                    <td>{x.contact}</td>
+                                    <td>{x.city}</td>
+                                    <td>{x.state}</td>
+                                    <td>{x.zipcode}</td>
+                                    <td>
+                                        <button className="btn btn-warning me-1">Edit</button>
+                                        <button className=" btn btn-danger">Delete</button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+           </table>
+        </>
+    )
+}
+export default Show;
