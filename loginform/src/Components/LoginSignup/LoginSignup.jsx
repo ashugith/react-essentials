3f97301dd1d25ace76e03c6a66c03b2527bc09e7
Ashu Singh <singh.ashu04072001@gmail.com>
2024-02-25 01:41:59 +0530

    Commit to change in login signup form
    Committing to change in login signup form

--- loginform/src/Components/LoginSignup/LoginSignup.jsx
+++ loginform/src/Components/LoginSignup/LoginSignup.jsx
@@ -15,17 +15,22 @@ const LoginSignup = ()  => {
         <div className="inputs">
             <div className="input">
                 <img src={user_icon} alt="user_icon"/>
-                <input type="text"/>
+                <input type="text" placeholder='Name'/>
                 </div>
 
                 <div className="input">
                 <img src={email_icon} alt="email_icon"/>
-                <input type="email"/>
+                <input type="email" placeholder='Email Id'/>
                 </div>
 
                 <div className="input">
                 <img src={password_icon} alt="password_icon"/>
-                <input type="password"/>
+                <input type="password" placeholder='Password'/>
+            </div>
+            <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
+            <div className="submit-container">
+                <div className="submit">Sign Up</div>
+                <div className="submit">Login</div>
             </div>
         </div>
       
