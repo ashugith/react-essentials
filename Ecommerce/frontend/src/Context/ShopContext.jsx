4750ac2299fa0d19471e9ca3cffe96932a2cd39a
Ashu Singh <singh.ashu04072001@gmail.com>
2024-02-26 01:17:19 +0530

    Commit to change in Ecommerce project.
    Committing to change in Ecommerce project.

--- Ecommerce/frontend/src/Context/ShopContext.jsx
+++ Ecommerce/frontend/src/Context/ShopContext.jsx
@@ -12,3 +12,4 @@ const ShopContextProvider = (props) => {
   )
 }
 export default ShopContextProvider;
+
--- loginsignup/src/components/LoginForm.js
+++ loginsignup/src/components/LoginForm.js
@@ -3,9 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function LoginForm ()  {
   return (
-    <div className='wrapper d-flex align-items-center justify-content-center w-100'>
+    <div className='wrapper bg-dark d-flex align-items-center justify-content-center w-100'>
     <div className='login'>
       <h2 className='mb-3'>Login Form</h2>
+      <form>
       <div className='form-group mb-2'>
         <label htmlFor='email'>Email Address</label>
         <input type='email'></input>
@@ -23,6 +24,7 @@ function LoginForm ()  {
 
       </div>
       <button type='submit' className='btn btn-success mt-2'>SIGN IN</button>
+      </form>
       
     </div>
     </div>
