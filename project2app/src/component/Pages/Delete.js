5a19af1d460d315d423d0652ae161c92c148f238
Ashu Singh <singh.ashu04072001@gmail.com>
2024-03-07 20:48:36 +0530

    Commit to change in project2.
    Committing to change in project2.

--- project2app/src/component/Pages/Delete.js
+++ project2app/src/component/Pages/Delete.js
@@ -1,5 +1,5 @@
-import { useEffect } from "react"
-import { useNavigate, useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { useNavigate, useParams ,NavLink } from "react-router-dom"
 
 const Delete = () => {
     const [customer , setCustomer] = useState({})
@@ -13,4 +13,21 @@ const Delete = () => {
     useEffect(() => {
         fetchData ()
     }, [])
+    function deleteData () {
+        axios.delete(`http://localhost:8080/customer/${customerID}`)
+        alert("Data Deleted !!!")
+        navigate('/show')
+    }
+    return (
+        <>
+        <form onSubmit={()=> deleteData()}>
+            <h2>Do You Really Want To Delete Record For</h2>
+            <h2>ID :- {customer.id}</h2>
+            <h2>Name :- {customer.name}</h2>
+            <button type="submit" className="btn btn-outline-success fs-3 me-3">YES</button>
+            <NavLink to={'/show'}><button type="button" className="btn btn-outline-warning fs-3">NO</button></NavLink>
+        </form>
+        </>
+    )
 }
+export default Delete;
