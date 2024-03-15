356b1f9d99f232fe50e461fe80c8ac7366d97c46
Ashu Singh <singh.ashu04072001@gmail.com>
2024-03-15 20:28:48 +0530

    Commit to change in food-app
    Committing to change in food-app.

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -1,11 +1,13 @@
+import { BrowserRouter as Router , Routes , Route } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-
-     
-    </div>
+    <Router>
+      <Routes>
+        <Route/>
+      </Routes>
+    </Router>
   );
 }
 
