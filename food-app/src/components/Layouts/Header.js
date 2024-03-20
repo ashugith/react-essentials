f796e23df9a5e354dde2ee5c08a93e83f90b79d2
Ashu Singh <singh.ashu04072001@gmail.com>
2024-03-20 11:39:48 +0530

    Commit to change in header.js
    Committing to change in header.js file

--- food-app/src/components/Layouts/Header.js
+++ food-app/src/components/Layouts/Header.js
@@ -5,23 +5,42 @@ import {Container, Nav , Navbar} from "react-bootstrap";
 function Header() {
   return (
     <>
-     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+     <Navbar bg="light" data-bs-theme="light">
       <Container>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className='me-auto'>
           
             <Nav.Link href="#features">Features</Nav.Link>
+      
             <Nav.Link href="#pricing">Pricing</Nav.Link>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
+          
+          
+              
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    <br/>
+    <Navbar bg="light" data-bs-theme="light">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className='me-auto'>
+          
+            <Nav.Link href="#features">Features</Nav.Link>
+      
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          
+          
+              
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
     </>
   )
 }
