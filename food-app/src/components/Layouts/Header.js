9115e535fe1bbc445eeab41ea7e5ebb6a751485d
Ashu Singh <singh.ashu04072001@gmail.com>
2024-03-20 09:39:36 +0530

    Commit to change in Header.js file.
    Committing to change in Header.js file.

--- food-app/src/components/Layouts/Header.js
+++ food-app/src/components/Layouts/Header.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import "../../styles/HeaderStyle.css"
+import {Container, Nav , Navbar} from "react-bootstrap";
 
 function Header() {
   return (
-    <div>
-      Header
-      
-    </div>
+    <>
+
+<Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <br />
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   )
 }
 
