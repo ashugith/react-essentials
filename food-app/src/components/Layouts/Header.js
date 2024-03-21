d368443a8ff92c35e3753bcf391777478265650e
Ashu Singh <singh.ashu04072001@gmail.com>
2024-03-21 20:38:03 +0530

    Commit to change in header.js file
    Committing to change in header.js file.

--- food-app/src/components/Layouts/Header.js
+++ food-app/src/components/Layouts/Header.js
@@ -20,27 +20,9 @@ function Header() {
               
           </Nav>
         </Navbar.Collapse>
+        
       </Container>
     </Navbar>
-    <br/>
-    <Navbar bg="light" data-bs-theme="light">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className='me-auto'>
-          
-            <Nav.Link href="#features">Features</Nav.Link>
-      
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          
-          
-              
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-
     </>
   )
 }
