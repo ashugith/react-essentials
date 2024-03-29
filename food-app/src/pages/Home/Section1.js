b23f1816b2a4deb616778807ea4783c87d1f2d5e
Ashu Singh <singh.ashu04072001@gmail.com>
2024-03-29 17:38:43 +0530

    Commit to change in Section1 file.
    Committing to change in section1 file.

--- food-app/src/pages/Home/Section1.js
+++ food-app/src/pages/Home/Section1.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Container, Row , Col } from 'react-bootstrap'
+import Burger from "../../assets/hero/hero-2.png"
 
 function Section1() {
   return (
-    <section className='hero'>
+    <section className='hero_section'>
+      <Container>
+        <Row>
+          <Col lg={7} className="mb-5 mb-lg-0">
+            <div className='position:relative'>
+                <img src={Burger} className='img-fluid' alt="Hero"/>
+                <div className='price_badge'>
+                  <div className='badge_text'>
+                    <h4 className='h4_xs'>Only</h4>
+                    <h4 className='h3_lg'>$6.99</h4>
+                    
+                  </div>
+                </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
 
     </section>
   )
