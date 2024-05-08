23258e06e50b97709eff0a7499f03169b3ab9bbf
Ashu Singh <singh.ashu04072001@gmail.com>
2024-05-09 01:24:16 +0530

    change

--- SpaceX_Dashboard-main/SpaceX_Dashboard-main/src/Components/Head.jsx
+++ SpaceX_Dashboard-main/SpaceX_Dashboard-main/src/Components/Head.jsx
@@ -4,9 +4,9 @@ import React from "react";
 const Head = () => {
   return (
     <>
-      <nav className="w-full fixed pl-2 bg-[#001F54] z-50">
+      <nav className="w-full fixed pl-2 bg-[#001F58] z-50">
         <div className="relative sm:mb-0 text-xs  py-4 ">
-            <span className="text-white text-sm font-semibold ">Hello Varun ,</span>
+            <span className="text-white text-sm font-semibold ">Hello Ashu! ,</span>
         </div>
       </nav>
     </>
