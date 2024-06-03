a92113c90152377b00bb0a4f9c4f8c2621152167
Ashu Singh <singh.ashu04072001@gmail.com>
2024-06-03 13:34:11 +0530

    Commit to change in Frontend project.
    Committing to change in frontend project.

--- Ecommerce/frontend/src/Item/Item.jsx
+++ Ecommerce/frontend/src/Item/Item.jsx
@@ -1,25 +1,14 @@
-import React from 'react'
-import './Item.css'
+import React from 'react';
+import './Item.css';
 
 const Item = (props) => {
   return (
     <div className='item'>
-        <img src={props.image} alt="" />
-        <p>{props.name}</p>
-        <div className="item-prices">
-            <div className="item-price-new">
-               $ {props.new_price}
-
-            </div>
-            <div className="item-price-old">
-                ${props.old_price}
-                
-            </div>
-        </div>
-
-      
+        <div>{props.name}</div>
+        <div>{props.old_price}</div>
+        <img src={props.image}></img>
     </div>
   )
 }
 
-export default Item
+export default Item;
--- Ecommerce/frontend/src/Pages/ShopCategory.jsx
+++ Ecommerce/frontend/src/Pages/ShopCategory.jsx
@@ -2,12 +2,14 @@ import React,{useContext} from 'react'
 import './CSS/ShopCategory.css'
 import { ShopContext } from '../Context/ShopContext'
 import dropdown_icon from '../Assets/dropdown_icon.png'
-import Item from '../Item/Item'
+import Item from '../Item/Item';
 
 
 
 const ShopCategory = (props) => {
   const {all_product} = useContext(ShopContext);
+  // console.log(all_product);
+  // console.log(props)
   return (
     <div className='shop-category'>
       <img src={props.banner} alt="" />
@@ -20,13 +22,16 @@ const ShopCategory = (props) => {
         </div>
       </div>
       <div className="shopcategory-products">
-      {all_product.map((item , i)=>{
-  if (props.category===item.category) {
-    return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-  } else {
-    return null;
-  }
-})}
+        {all_product.map((item, i) =>{
+          if(item.category === props.category)
+          {
+           return<Item key={i} id={item.id} name={item.name} old_price={item.old_price} image={item.image}/>
+          }
+          else{
+            return null;
+          }
+
+        })}
       </div>
       
     </div>
